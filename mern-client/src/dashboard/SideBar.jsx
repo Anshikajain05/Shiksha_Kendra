467fa90df06365cba3a52ff7db1380d527b0c5a1
anshika jain <Y226101005@dhsgsu.edu.in>
2026-03-25 01:08:19 +0530

    Update SideBar.jsx

--- mern-client/src/dashboard/SideBar.jsx
+++ mern-client/src/dashboard/SideBar.jsx
@@ -1,57 +1,57 @@
-
-
 import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems, SidebarLogo } from "flowbite-react";
-import { BiBuoy } from "react-icons/bi";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloudUpload, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
-import userImg from "../assets/profile.jpg"
+import { HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloudUpload, HiShoppingBag, HiTable, HiUser } from "react-icons/hi";
+import userImg from "../assets/profile.jpg";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import { Link } from "react-router-dom";
+
 const SideBar = () => {
-  const {user} = useContext(AuthContext)
-    return (
-		<div color='white'>
-			<Sidebar>
-				<SidebarLogo href='#' img={userImg} imgAlt='Flowbite logo'>
-					<p>{user?.displayName || "Admin"}</p>
-				</SidebarLogo>
-				<SidebarItems>
-					<SidebarItemGroup>
-						<SidebarItem href='/admin/dashboard' icon={HiChartPie}>
-							Dashboard
-						</SidebarItem>
-						<SidebarItem href='/admin/dashboard/Upload' icon={HiOutlineCloudUpload}>
-							Uploadbook
-						</SidebarItem>
-						<SidebarItem href='/admin/dashboard/ManageBook' icon={HiInbox}>
-							ManageBook
-						</SidebarItem>
-						<SidebarItem href='/admin/dashboard/users' icon={HiUser}>
-							Users
-						</SidebarItem>
-						<SidebarItem href='#' icon={HiShoppingBag}>
-							Materials
-						</SidebarItem>
-						<SidebarItem href='/login' icon={HiArrowSmRight}>
-							Sign In
-						</SidebarItem>
-						<SidebarItem href='/logout' icon={HiTable}>
-							Log Out
-						</SidebarItem>
-					</SidebarItemGroup>
-					{/* <SidebarItemGroup>
-          <SidebarItem href="#" icon={HiChartPie}>
-            Upgrade to Pro
-          </SidebarItem>
-          <SidebarItem href="#" icon={HiViewBoards}>
-            Documentation
-          </SidebarItem>
-          <SidebarItem href="#" icon={BiBuoy}>
-            Help
-          </SidebarItem>
-        </SidebarItemGroup>*/}
-				</SidebarItems>
-			</Sidebar>
-		</div>
-    );
-}
-export default SideBar
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div>
+      <Sidebar>
+        <SidebarLogo href="#" img={userImg} imgAlt="logo">
+          <p>{user?.displayName || "Admin"}</p>
+        </SidebarLogo>
+
+        <SidebarItems>
+          <SidebarItemGroup>
+            <SidebarItem icon={HiChartPie}>
+              <Link to="/admin/dashboard">Dashboard</Link>
+            </SidebarItem>
+
+            <SidebarItem icon={HiOutlineCloudUpload}>
+              <Link to="/admin/dashboard/Upload">Uploadbook</Link>
+            </SidebarItem>
+
+            <SidebarItem icon={HiInbox}>
+              <Link to="/admin/dashboard/ManageBook">ManageBook</Link>
+            </SidebarItem>
+
+            <SidebarItem icon={HiUser}>
+              <Link to="/admin/dashboard/users">Users</Link>
+            </SidebarItem>
+
+            <SidebarItem icon={HiShoppingBag}>
+              Materials
+            </SidebarItem>
+
+            {/* Conditional Auth UI */}
+            {user ? (
+              <SidebarItem icon={HiTable}>
+                <Link to="/logout">Log Out</Link>
+              </SidebarItem>
+            ) : (
+              <SidebarItem icon={HiArrowSmRight}>
+                <Link to="/login">Sign In</Link>
+              </SidebarItem>
+            )}
+          </SidebarItemGroup>
+        </SidebarItems>
+      </Sidebar>
+    </div>
+  );
+};
+
+export default SideBar;
